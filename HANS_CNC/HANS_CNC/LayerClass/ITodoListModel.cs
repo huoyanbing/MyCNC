38cb5fc9214a6512c342dce456b204a51e0deae1
yang ou <380462254@qq.com>
2020-01-15 18:01:03 +0800

    2020.1.15

---
+++ HANS_CNC/HANS_CNC/LayerClass/ITodoListModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HANS_CNC.LayerClass
+{
+    interface ITodoListModel
+    {
+        DataSet GetTodoList();
+        int AddTodoListItem();
+        bool RemoveTodoListItem(int recordID);
+        void UpdateXMLDoc(DataSet newTodoListDataSet);
+    }
+}
